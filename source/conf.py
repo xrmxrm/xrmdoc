c9f83839843718e7db850a8a41a363fba88dec50
SymbioRichard <richard@symb.io>
2020-03-13 10:14:25 -0700

    More imitation

--- source/conf.py
+++ source/conf.py
@@ -1,6 +1,7 @@
 # Configuration file for the Sphinx documentation builder.
+# -*- coding: utf-8 -*-
 #
-# This file only contains a selection of the most common options. For a full
+# This file contains a selection of the most common options. For a full
 # list see the documentation:
 # https://www.sphinx-doc.org/en/master/usage/configuration.html
 
@@ -14,13 +15,11 @@
 # import sys
 # sys.path.insert(0, os.path.abspath('.'))
 
-
 # -- Project information -----------------------------------------------------
 
 project = 'xrmdoc'
 copyright = '2020, Symbio Robotics'
 author = 'Richard Mateosian'
-
 
 # -- General configuration ---------------------------------------------------
 
@@ -36,23 +35,63 @@ extensions = [
     'sphinx.ext.todo',
 ]
 
+# Support reStructuredText and Markdown.
+source_suffix = ['.rst', '.md']
+
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
-exclude_patterns = []
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
+# The name of the Pygments (syntax highlighting) style to use.
+pygments_style = 'sphinx'
 
 # -- Options for HTML output -------------------------------------------------
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+html_show_sourcelink = False
+html_show_sphinx = False
+html_theme = 'sphinx_rtd_theme'
+html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
+
+# Theme options are theme-specific and customize the look and feel of a theme
+# further.  For a list of options available for each theme, see the
+# documentation.
+
+html_theme_options = {
+    'logo_only': True,
+    'navigation_depth': 4
+}
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
+
+html_context = {
+    'css_files': [
+        '_static/theme_overrides.css',  # override wide tables in RTD theme
+    ]
+}
+
+intersphinx_mapping = {
+    'python': ('https://docs.python.org/3.7/', None)
+}
+autodoc_default_flags = ['members', 'show-inheritance']
+
+# Napoleon
+
+napoleon_google_docstring = True
+napoleon_use_param = True
+napoleon_use_rtype = True
+
+# External Links
+# Define variables here in the form 'name': (URL, None)
+
+extlinks = {
+}
